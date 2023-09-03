c2433c6c2dd32fc27aa0c0c5b54772f075e0c8b2
tianyicheng <1573496757@qq.com>
2023-09-03 16:50:39 +0800

    feat：封装请求

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,61 +1,80 @@
 <template>
-    <div>
-        <!-- 首页展示轮播图 -->
-        <Carousel />
-        <!-- 首页输入框 -->
-        <Search />
-        <!-- 底部展示医院结构 -->
-        <el-row :gutter="50">
-            <el-col :span="18">
-                <!-- 展示医院等级 -->
-                <Level/>
-                <!-- 展示医院地区 -->
-                <Region/>
-                <!-- 展示医院卡片 -->
-                <div class="cards">
-                    <Card v-for="item in 10" :key="item" class="card"/>
-                </div>
-                <!-- 展示分页 -->
-                <Pagination/>
-            </el-col>
-            <el-col :span="6">
-                <!-- 展示右侧公告信息 -->
-                <Tip/>
-            </el-col>
-        </el-row>
-    </div>
+  <div>
+    <!-- 首页展示轮播图 -->
+    <Carousel />
+    <!-- 首页输入框 -->
+    <Search />
+    <!-- 底部展示医院结构 -->
+    <el-row :gutter="50">
+      <el-col :span="18">
+        <!-- 展示医院等级 -->
+        <Level />
+        <!-- 展示医院地区 -->
+        <Region />
+        <!-- 展示医院卡片 -->
+        <div class="cards">
+          <Card v-for="item in 10" :key="item" class="card" />
+        </div>
+        <!-- 展示分页 -->
+        <Pagination />
+      </el-col>
+      <el-col :span="6">
+        <!-- 展示右侧公告信息 -->
+        <Tip />
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script setup lang="ts">
 // 引入轮播图组件
-import Carousel from './carousel/index.vue';
+import Carousel from "./carousel/index.vue";
 // 引入首页搜索组件
 // @ts-ignore
-import Search from './search/index.vue'
+import Search from "./search/index.vue";
 // 展示首页医院等级组件
-import Level from './level/index.vue'
+import Level from "./level/index.vue";
 // 引入首页医院地区组件
-import Region from './region/index.vue'
+import Region from "./region/index.vue";
 // 引入首页医院卡片组件
-import Card from './card/index.vue'
+import Card from "./card/index.vue";
 // 引入首页右侧提示公告组件
 // @ts-ignore
-import Tip from './tip/index.vue'
+import Tip from "./tip/index.vue";
 // 引入首页分页插件
-import Pagination from './pagination/index.vue'
+import Pagination from "./pagination/index.vue";
+
+import {reqHospital} from '@/api/home'
+import { onMounted, ref } from "vue";
+
+
+let hasHospitalArr = ref([])
+
+onMounted(() => {
+  getHospitalInfo();
+});
+
+const getHospitalInfo = async()=>{
+  let result = await reqHospital(1,10)
+  console.log(result)
+  if(result.data == 200){
+    hasHospitalArr.value = result.data.content
+  }
+}
+
 </script>
 
 
 <style scoped lang="scss">
-.cards{
-    margin-top: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    margin-bottom: 20px;
-    .card{
-        width: 48%;
-        margin-right: 15px;
-        margin-top: 10px;
-    }
+.cards {
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 20px;
+  .card {
+    width: 48%;
+    margin-right: 15px;
+    margin-top: 10px;
+  }
 }
 </style>
---
+++ src/utils/request.ts
@@ -0,0 +1,48 @@
+// 对于 axios 函数库进行二次封装
+// 目的 
+// 利用axios请求，响应拦截器功能
+// 请求拦截器中携带公共参数：token
+// 响应拦截器简化服务器返回数据处理http错误
+import axios from 'axios'
+import {ElMessage} from 'element-plus'
+const request = axios.create({
+    baseURL:'/api',
+    timeout:10000
+})
+
+// 请求拦截器
+request.interceptors.request.use((config) =>{
+    config.headers.a=111;
+    return config
+})
+
+// 响应拦截器
+request.interceptors.response.use((response)=>{
+    return response.data;
+},(error)=>{
+    let status = error.response.status;
+    switch (status){
+        case 404:
+            ElMessage({
+                type: 'error',
+                message:'请求路径无效'
+            })
+            break
+        case 500|501|502|503|504|505:
+            ElMessage({
+                type: 'error',
+                message:'服务器异常'
+            })
+            break
+        case 401:
+            ElMessage({
+                type: 'error',
+                message:'参数有误'
+            })
+            break
+    }
+    // 处理http错误
+    return Promise.reject(new Error(error.message))
+})
+
+export default request;
